69f2d7bd82e55f437d6710d1476ea243aab0b684
Cleetus14 <natorprime1@gmail.com>
2026-07-12 12:18:36 -0400

    learned Variables and Datatypes

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	printf("I like pizza!\n");
-	printf("It's really good!");
 
-		return 0;
+	char name[] = "Nick";
+	int age = 15;
+	float height = 5.7f;
+	char favoriteLetter = 'N';
+	int favoriteNumber = 79;
+
+	printf("Name: %s \n", name);
+	printf("Age: %d \n", age);
+	printf("Height: %f \n", height);
+	printf("Favorite Letter: %c \n", favoriteLetter);
+	printf("Favorite Number: %d \n", favoriteNumber);
+	return 0;
 }
