2c7c23e50911d7a2d7a408c09ecdc7f607b2bc02
Cleetus14 <natorprime1@gmail.com>
2026-07-13 19:48:18 -0400

    Learned user input (Practice Project #1)

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+    char name[50];
+    int age;
+    char favoriteProgrammingLanguage[50];
+    int favoriteNumber;
 
-	char name[] = "Nick";
-	int age = 15;
-	float height = 5.7f;
-	char favoriteLetter = 'N';
-	int favoriteNumber = 79;
+    printf("Enter Your Name: ");
+    scanf_s("%s", name, 50);
 
-	printf("Name: %s \n", name);
-	printf("Age: %d \n", age);
-	printf("Height: %f \n", height);
-	printf("Favorite Letter: %c \n", favoriteLetter);
-	printf("Favorite Number: %d \n", favoriteNumber);
-	return 0;
+    printf("Enter Your Age: ");
+    scanf_s("%d", &age);
+
+    printf("Enter Your Favorite Programming Language: ");
+    scanf_s("%s", favoriteProgrammingLanguage, 50);
+
+    printf("Enter Your Favorite Number: ");
+    scanf_s("%d", &favoriteNumber);
+
+    printf("\n--- Interview Results ---\n");
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Favorite Programming Language: %s\n", favoriteProgrammingLanguage);
+    printf("Favorite Number: %d\n", favoriteNumber);
+
+    return 0;
 }
