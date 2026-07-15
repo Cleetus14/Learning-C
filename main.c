e0282c3e667fc825c39e192520f0135255e8ef2b
Cleetus14 <natorprime1@gmail.com>
2026-07-15 12:01:57 -0400

    Mini Calculator V2 (With functions implemented)

--- main.c
+++ main.c
@@ -1,74 +1,68 @@
 #include <stdio.h>
 
+float add(float x, float y) {
+	return x + y;
+}
+
+float subtract(float x, float y) {
+	return x - y;
+}
+
+float multiply(float x, float y) {
+	return x * y;
+}
+
+float divide(float x, float y) {
+	return x / y;
+}
+
 int main()
 {
-	float balance = 1000.00;
-	char option;
-	float amountToDeposit = 0;
-	float amountToWithdraw = 0;
-	float newBalance = 0;
-	int pin = 1234;
+	float firstNumber;
+	float secondNumber;
+	float result;
+	char operation;
 
-	printf("Enter your pin to access your bank account: ");
-	scanf_s("%d", &pin);
+	printf("Enter first number: ");
+	scanf_s("%f", &firstNumber);
 
-	if (pin == 1234)
+	printf("Enter an operator (+, -, *, /): ");
+	scanf_s(" %c", &operation, 1);
+
+	printf("Enter second number: ");
+	scanf_s("%f", &secondNumber);
+
+	switch (operation)
 	{
-		printf("===== Bank Account =====\n");
-		printf("1. Check Balance\n");
-		printf("2. Deposit Money\n");
-		printf("3. Withdraw Money\n");
-		printf("4. Exit\n");
-
-		printf("Choose an option: \n");
-		scanf_s(" %c", &option, 1);
-	}
-	else
-	{
-		printf("Wrong Pin!\n");
-		printf("Try Again!\n");
-		return 1;
-	}
-
-	switch (option)
-	{
-	case '1':
-		printf("Your current balance is: $%.2f", balance);
+	case '+':
+		result = add(firstNumber, secondNumber);
+		printf("Result: %.3f\n", result);
 		break;
 
-	case '2':
-		printf("Enter an amount to deposit: $");
-		scanf_s("%f", &amountToDeposit);
-
-		if (amountToDeposit <= 0)
-		{
-			printf("Invalid amount.");
-		}
-		else {
-			newBalance = balance + amountToDeposit;
-			printf("New Balance: $%.2f", newBalance);
-			break;
-		}
-
-	case '3':
-		printf("Enter amount to withdraw: $");
-		scanf_s("%f", &amountToWithdraw);
-		if (amountToWithdraw > balance)
-		{
-			printf("Insufficient funds.");
-		}
-		else if (amountToWithdraw <= balance)
-		{
-			newBalance = balance - amountToWithdraw;
-			printf("New Balance: $%.2f", newBalance);
-			break;
-		}
-	case '4':
-		printf("Thank you for using the bank simulator. Have a good day!");
+	case '-':
+		result = subtract(firstNumber, secondNumber);
+		printf("Result: %.3f\n", result);
 		break;
-	
+
+	case '*':
+		result = multiply(firstNumber, secondNumber);
+		printf("Result: %.3f\n", result);
+		break;
+
+	case '/':
+		if (secondNumber == 0)
+		{
+			printf("Cannot divide by zero.\n");
+			return 1;
+		}
+
+		result = divide(firstNumber, secondNumber);
+		printf("Result: %.3f\n", result);
+		break;
+
 	default:
-		printf("Invalid option.");
+		printf("Invalid operator.\n");
+		return 1;
 	}
 
 	return 0;
